ed3cb54106c882e92f74a0148f45a8eace62543f
aalia021 <aalia9209@gmail.com>
2025-05-31 15:55:33 +0530

    onboarding page and onboarding flow

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,12 +7,13 @@ import {
   SignIn,
   SignInButton,
   UserButton,
+  useUser,
 } from "@clerk/clerk-react";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
   const [showSignIn, setShowSignIn] = useState(false);
-
+  const { user } = useUser();
   const [search, setSearch] = useSearchParams();
 
   useEffect(() => {
@@ -40,12 +41,14 @@ const Header = () => {
             </Button>
           </SignedOut>
           <SignedIn>
-            <Link to="/post-job">
-              <Button variant="destructive" className="rounded-full">
-                <PenBox size={20} className="mr-2" />
-                Post a job
-              </Button>
-            </Link>
+            {user.unsafeMetadata.role === "recruiter" && (
+              <Link to="/post-job">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a job
+                </Button>
+              </Link>
+            )}
             <UserButton
               appearance={{
                 elements: {
