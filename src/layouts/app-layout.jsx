3693e3aff946396c129d0bb890789382ea7a2074
aalia021 <aalia9209@gmail.com>
2025-05-31 12:57:35 +0530

    fixed ui issue

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -5,12 +5,15 @@ const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <main className="min-h-screen container">
-        <Header />
-        <Outlet />
+      <main className="min-h-screen">
+        <div className="container mx-auto px-4">
+          <Header />
+          <Outlet />
+        </div>
       </main>
+
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
+        Made with 💗 by Aalia Khan
       </div>
     </div>
   );
